4acc533da3314c2368f1a710ea62e454d7ce7fd0
justinrosales <jpnrosales@outlook.com>
2025-03-23 07:15:47 -0400

    add NavBar and Footer

--- app/components/footer/Footer.tsx
+++ app/components/footer/Footer.tsx
@@ -1,5 +1,66 @@
+import { MdFacebook } from "react-icons/md";
+import Container from "../Container";
+import FooterList from "./FooterList";
+import Link from "next/link";
+import {
+  AiFillInstagram,
+  AiFillTwitterCircle,
+  AiFillYoutube,
+} from "react-icons/ai";
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="bg-slate-700 text-slate-200 text-sm mt-16">
+      <Container>
+        <div className="flex flex-col md:flex-row justify-between pt-16 pb-8">
+          <FooterList>
+            <h3 className="text-base font-bold mb-2">Shop Categories</h3>
+            <Link href={"#"}>Phones</Link>
+            <Link href={"#"}>Laptops</Link>
+            <Link href={"#"}>Desktops</Link>
+            <Link href={"#"}>Watches</Link>
+            <Link href={"#"}>TVs</Link>
+            <Link href={"#"}>Accessories</Link>
+          </FooterList>
+          <FooterList>
+            <h3 className="text-base font-bold mb-2">Customer Service</h3>
+            <Link href={"#"}>Contact Us</Link>
+            <Link href={"#"}>Shipping Policy</Link>
+            <Link href={"#"}>Returns and Exchanges</Link>
+            <Link href={"#"}>FAQs</Link>
+          </FooterList>
+          <div className="w-full md:w-1/3 mb-6 md:mb-0">
+            <h3 className="text-base folt-bold mb-2">About Us</h3>
+            <p className="mb-2">
+              We are the best in the business. Surrounded by a team of talented
+              individuals who dedicate their life serving Ken Leywin, the
+              honored one.
+            </p>
+            <p>
+              &copy; {new Date().getFullYear()} E-Commerce. All rights reserved
+            </p>
+          </div>
+          <FooterList>
+            <h3 className="text-base folt-bold mb-2">Follow Us</h3>
+            <div className="flex gap-2">
+              <Link href={"#"}>
+                <MdFacebook size={24} />
+              </Link>
+              <Link href={"#"}>
+                <AiFillTwitterCircle size={24} />
+              </Link>
+              <Link href={"#"}>
+                <AiFillInstagram size={24} />
+              </Link>
+              <Link href={"#"}>
+                <AiFillYoutube size={24} />
+              </Link>
+            </div>
+          </FooterList>
+        </div>
+      </Container>
+    </div>
+  );
 };
 
 export default Footer;
