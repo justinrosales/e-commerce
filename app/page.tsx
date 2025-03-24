95b41a8ebda62a6df313b304bf6a0fb47c0c20ff
justinrosales <rosalesjpn@gmail.com>
2025-03-23 20:59:09 -0400

    add HomeBanner

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,14 @@
-import Image from "next/image";
+import Container from "./components/Container";
+import HomeBanner from "./components/HomeBanner";
 
 export default function Home() {
-  return <div>Ecommerce</div>;
+  return (
+    <div className="p-8">
+      <Container>
+        <div>
+          <HomeBanner />
+        </div>
+      </Container>
+    </div>
+  );
 }
