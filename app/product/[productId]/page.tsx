992949441df3e282fb522c43758f2269980a8515
justinrosales <rosalesjpn@gmail.com>
2025-03-25 23:48:13 -0400

    create page and routes

---
+++ app/product/[productId]/page.tsx
@@ -0,0 +1,10 @@
+interface IParams {
+  productId?: string;
+}
+
+const Product = ({ params }: { params: IParams }) => {
+  //console.log('params', params);
+  return <div>Product Page</div>;
+};
+
+export default Product;
