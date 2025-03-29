e73de5d1ecf6eaa80c06f20179a0b7d456074cd5
justinrosales <rosalesjpn@gmail.com>
2025-03-29 02:23:25 -0400

    add product details

--- app/product/[productId]/page.tsx
+++ app/product/[productId]/page.tsx
@@ -1,10 +1,21 @@
+import Container from "@/app/components/Container";
+import ProductDetails from "./ProductDetails";
+import { product } from "@/utils/product";
+
 interface IParams {
   productId?: string;
 }
 
 const Product = ({ params }: { params: IParams }) => {
-  //console.log('params', params);
-  return <div>Product Page</div>;
+  console.log("params", params);
+
+  return (
+    <div className="p-8">
+      <Container>
+        <ProductDetails product={product} />
+      </Container>
+    </div>
+  );
 };
 
 export default Product;
